ae1a3de1393cbcce614f99099f1fc4eb8df4119b
Timothy Babatunde Akanbi <97198113+TimothyBabatu13@users.noreply.github.com>
2023-01-06 11:35:08 +0100

    Add files via upload
    Work o

---
+++ app.js
@@ -0,0 +1,800 @@
+/* Array of questions and Answers */
+let questions = [
+  {
+    question: "The early bird cathes the...? ",
+    answer: [{
+       text: "...cat", correct: false
+    },
+    {
+      text: "...ant", correct : false
+    },
+    {
+      text: "...monkey", correct: false
+    },
+    {
+       text: "...worm", correct : true 
+    }]
+  },
+  
+  
+    {
+    question: "What is national flower of Japan? Hint: it is pink coloured flowers of cherry trees.",
+    answer: [{
+       text: "snowdrop", correct: false
+    },
+    {
+      text: "tulip", correct : false
+    },
+    {
+      text: "mimosa", correct: false
+    },
+    {
+       text: "cherry blossom", correct : true
+    }]
+    },
+    
+    
+    
+   {
+    question: "What is the last name of former World No. 1 tennis champions Venus and Serena?",
+    answer: [
+      {
+       text: "Williams", correct: true
+      },
+      {
+      text: "Davenport", correct : false
+        
+      },
+      {
+      text: "Kournikova", correct: false
+        
+      },
+      {
+       text: "Capriati", correct : false
+       
+      }]
+    },
+    
+    
+    {
+    question: "Whic month has an extra day during a leap year?",
+    answer: [
+      {
+       text: "January", correct: false
+      },
+      {
+      text: "February", correct : true
+      },
+      {
+      text: "May", correct: false
+      },
+      {
+       text: "March", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "To type a simple letter, a computer user will use a:",
+    answer: [
+      {
+       text: "Spreedsheet", correct: false
+      },      
+      {
+      text: "Food Processor", correct : false
+      },
+      {
+      text: "Time Processor", correct: false
+      },
+      {
+       text: "Word Processor", correct : true
+      }]
+    },
+    
+    
+    {
+    question: "Tinky Winky, Po, Dipsy and Laa-Laa are characters on what children's television program",
+    answer: [
+      {
+       text: "Blue Peter", correct: false
+      },
+      {
+      text: "Sesame Street", correct : false
+      },
+      {
+      text: "Barney and Friends", correct: false
+      },
+      {
+       text: "Teletubbies", correct : true
+      }]
+    },
+    
+    
+    
+    {
+    question: "Which of the following animals does not come from egg?",
+    answer: [
+      {
+       text: "crocodile", correct: false
+      },
+      {
+      text: "chicken", correct : false
+      },
+      {
+      text: "tiger", correct: true
+      },
+      {
+       text: "turtle", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "If you are superstitious, where do you throw salt if you spill it?",
+    answer: [
+      {
+       text: "Under your armpit", correct: false
+      },
+      {
+      text: "Over your shoulder", correct : true
+      },
+      {
+      text: "Glass of water", correct: false
+      },
+      {
+       text: "In your hair", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "What association did the Village People perform a song about?",
+    answer: [
+      {
+       text: "NRA", correct: false
+      },
+      {
+      text: "AHA", correct : false
+      },
+      {
+      text: "AMA", correct: false
+      },
+      {
+       text: "YMCA", correct : true
+      }]
+    },
+    
+    
+    {
+    question: "What vegetables gives Popeye superhuman strength",
+    answer: [
+      {
+       text: "Carrots", correct: false
+      },
+      {
+      text: "Spinach", correct : true
+      },
+      {
+      text: "Lettuce", correct: false
+      },
+      {
+       text: "Cabbage", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "From which animal do we obtain bacon?",
+    answer: [
+      {
+       text: "Pig", correct: true
+      },
+      {
+      text: "Chicken", correct : false
+      },
+      {
+      text: "Cow", correct: false
+      },
+      {
+       text: "Sheep", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "What colors are the stars on the American flag",
+    answer: [
+      {
+       text: "white", correct: true
+      },
+      {
+      text: "red", correct : false
+      },
+      {
+      text: "blue", correct: false
+      },
+      {
+       text: "yellow", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "What is the name of the fairy that aids Peter Pan in J.M. Barrie's play and novel?",
+    answer: [
+      {
+       text: "Puck", correct: false
+      },
+      {
+      text: "Tinkerbell", correct : true
+      },
+      {
+      text: "Shobijin", correct: false
+      },
+      {
+       text: "Navi", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "What do you call a piece or a set of clothing worn for swimming?",
+    answer: [
+      {
+       text: "hat", correct: false
+      },
+      {
+      text: "swimsuit", correct : true
+      },
+      {
+      text: "tie", correct: false
+      },
+      {
+       text: "raincoat", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "According to superstition, what is unlucky to walk under?",
+    answer: [
+      {
+       text: "Ladder", correct: true
+      },
+      {
+      text: "Cloud", correct : false
+      },
+      {
+      text: "Bridge", correct: false
+      },
+      {
+       text: "Rainbow", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "What do you call a man that works in the science industry? They do many experiments",
+    answer: [
+      {
+       text: "secretary", correct: false
+      },
+      {
+      text: "scientist", correct : true
+      },
+      {
+      text: "fortune-teller", correct: false
+      },
+      {
+       text: "florist", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "The triathlon is made up of running, swimming and which other even?",
+    answer: [
+      {
+       text: "Fencing", correct: false
+      },
+      {
+      text: "Cycling", correct : true
+      },
+      {
+      text: "Horse riding", correct: false
+      },
+      {
+       text: "Shooting", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "Which word describes a type of headdress?",
+    answer: [
+      {
+       text: "Hiab", correct: false
+      },
+      {
+      text: "Abaya", correct : false
+      },
+      {
+      text: "Turban", correct: true
+      },
+      {
+       text: "Bindi", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "Pikachu is a ...?",
+    answer: [
+      {
+       text: "Smurf", correct: false
+      },
+      {
+      text: "Teletubby", correct : false
+      },
+      {
+      text: "Pokémon ", correct: true
+      },
+      {
+       text: "Simpson", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "Which of these do not produce personal computers?",
+    answer: [
+      {
+       text: "Apple", correct: false
+      },
+      {
+      text: "Dell", correct : false
+      },
+      {
+      text: "Nokia", correct: true
+      },
+      {
+       text: "Lenovo", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "(2 + 2) × (3 + 2) + 4 = ?",
+    answer: [
+      {
+       text: "10", correct: false
+      },
+      {
+      text: "18", correct : false
+      },
+      {
+      text: "14", correct: false
+      },
+      {
+       text: "24", correct : true
+      }]
+    },
+    
+    
+    
+    {
+    question: "Who has been the leader of Cuba since 1959?",
+    answer: [
+      {
+       text: "Fidel Castro", correct: true
+      },
+      {
+      text: "Cipriano Castro", correct : false
+      },
+      {
+      text: "Evo Morales", correct: false
+      },
+      {
+       text: "Hugo Chávez", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "Citrus fruits are good sources of which vitamin?",
+    answer: [
+      {
+       text: "Vitamin A", correct: false
+      },
+      {
+      text: "Vitamin C", correct : true
+      },
+      {
+      text: "Vitamin E", correct: false
+      },
+      {
+       text: "Vitamin B", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "What is the name of the sheep used to describe a person that does not fit into the family?",
+    answer: [
+      {
+       text: "Blue", correct: false
+      },
+      {
+      text: "Black", correct : true
+      },
+      {
+      text: "Pink", correct: false
+      },
+      {
+       text: "Red", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "What is Winnie the Pooh's favourite thing to eat?",
+    answer: [
+      {
+       text: "Grass", correct: false
+      },
+      {
+      text: "Seaweed", correct : false
+      },
+      {
+      text: "Honey", correct: true
+      },
+      {
+       text: "Hamburgers", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "Which country is home to flamenco dancing",
+    answer: [
+      {
+       text: "Russia", correct: false
+      },
+      {
+      text: "Germany", correct : false
+      },
+      {
+      text: "Norway", correct: false
+      },
+      {
+       text: "Spain", correct : true
+      }]
+    },
+    
+    
+    {
+    question: "Diamonds are a girl's ...?",
+    answer: [
+      {
+       text: "...big dream", correct: false
+      },
+      {
+      text: "...best friend", correct : true
+      },
+      {
+      text: "...best buddie", correct: false
+      },
+      {
+       text: "...worst enemy", correct : false
+      }]
+    },
+    
+    
+    
+    {
+    question: "What do you call a relationship with someone you've met online? ",
+    answer: [
+      {
+       text: "eDAE", correct: false
+      },
+      {
+      text: "ebay", correct : false
+      },
+      {
+      text: "eBye-bye", correct: false
+      },
+      {
+       text: "eBAE", correct : true
+      }]
+    },
+    
+    
+    {
+    question: "What do you call a person that makes money from selling land for development?",
+    answer: [
+      {
+       text: "real estate agent", correct: true
+      },
+      {
+      text: "hairdresser", correct : false
+      },
+      {
+      text: "secret agent", correct: false
+      },
+      {
+       text: "receptionist", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "What actress famously sang Happy Birthday to President John F. Kennedy?",
+    answer: [
+      {
+       text: "Marilyn Monroe", correct: true
+      },
+      {
+      text: "Marlene Dietrich", correct : false
+      },
+      {
+      text: "Grace Kelly", correct: false
+      },
+      {
+       text: "Jayne Mansfield", correct : false
+      }]
+    },
+    
+    
+   
+    {
+    question: "Where there's a will...?",
+    answer: [
+      {
+       text: "...there's a way", correct: true
+      },
+      {
+      text: "...there's a grace", correct : false
+      },
+      {
+      text: "...there's a door", correct: false
+      },
+      {
+       text: "...there's a death", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "How many of each kind of animal ambled into Noah's Ark, according to the Biblical account",
+    answer: [
+      {
+       text: "2", correct: true
+      },
+      {
+      text: "1", correct : false
+      },
+      {
+      text: "3", correct: false
+      },
+      {
+       text: "4", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "What do you call a person that studies the star and universe?",
+    answer: [
+      {
+       text: "accountant", correct: false
+      },
+      {
+      text: "magician", correct : false 
+      },
+      {
+      text: "translator", correct: false
+      },
+      {
+       text: "astronomer", correct : true
+      }]
+    },
+    
+    
+    {
+    question: "What do you call a substance that is used to or serves to provide color?",
+    answer: [
+      {
+       text: "pigment", correct: true
+      },
+      {
+      text: "art", correct : false
+      },
+      {
+      text: "peppermint", correct: false
+      },
+      {
+       text: "pygmalion", correct : false
+      }]
+    },
+    
+    {
+    question: "What does letter 'A' in Nathaniel Hawthorne's 'The Scarlet Letter' stand for?",
+    answer: [
+      {
+       text: "Avenger", correct: false
+      },
+      {
+      text: "Adulteress", correct : true
+      },
+      {
+      text: "Aggressor", correct: false
+      },
+      {
+       text: "Atrocious", correct : false
+      }]
+    },
+    
+    
+     {
+    question: "If your zodiac sign is a Leo, in what month were you born? ",
+    answer: [
+      {
+       text: "May or June", correct: false
+      },
+      {
+      text: "December or January", correct : false
+      },
+      {
+      text: "September or October", correct: false
+      },
+      {
+       text: "July or August", correct : true
+      }]
+    },
+    
+    
+    {
+    question: "Which Civil War general is known by his nickname 'StoneWall'?",
+    answer: [
+      {
+       text: "Thomas Jackson", correct: true
+      },
+      {
+      text: "Richard S. Ewell ", correct : false
+      },
+      {
+      text: "Turner Ashby ", correct: false
+      },
+      {
+       text: "Robert E. Lee", correct : false
+      }]
+    },
+    
+    
+    {
+    question: "Which of these is NOT a murder weapon in the board game 'Clue'?",
+    answer: [
+      {
+       text: "Poison", correct: true
+      },
+      {
+      text: "Rope", correct : false
+      },
+      {
+      text: "Dagger", correct: false
+      },
+      {
+       text: "Lead Pipe", correct : false
+      }]
+    } 
+  ]
+
+let questionContainer = document.querySelector('#container');
+
+let displayScoreContainer = document.querySelector('#second-page');
+
+let questionScreen = document.querySelector('#question-container');
+
+let anser = document.querySelectorAll('.answer-space');
+
+let btnbtn = document.querySelectorAll('.answer-btn');
+
+let number = document.querySelector('#number-container span');
+
+let nextBtn = document.querySelector('#next');
+
+let refresh = document.querySelector('#start-afresh');
+
+/* Choosing random number between 0 and questions.length && assigning it to n*/
+let n = Math.floor(Math.random() * (questions.length -1)); 
+
+/*  score is used to know the amount of questions got
+count is used to make sure the users doesn't answer questions more than 20.
+*/
+let score = 0;
+let count = 1;
+
+/* Hiding the page that displays number of answers gotten correctly*/
+displayScoreContainer.classList.add('hide');
+
+
+function hideAndDisplay(){
+  container.classList.add('hide');
+  displayScoreContainer.classList.remove('hide');
+}
+refresh.onclick = ()=>{
+  window.location.reload();
+}
+
+nextBtn.onclick = ()=>{
+  btnbtnColor("white");
+  count++;
+  n++;
+  
+  number.innerHTML = count;
+  
+  (count == 20) ? nextBtn.innerHTML = "Submit" : null;
+  
+  (next.innerHTML == "Submit" && count == 21) ? hideAndDisplay() : null;
+  
+  (n > questions.length - 1) ? n = 0 : null;
+  
+  display();
+}
+
+function btnbtnColor(x){
+  for(let i = 0; i < btnbtn.length; i++){
+    btnbtn[i].style.color = x;
+  }
+}
+
+function display(){
+  questionScreen.innerHTML = questions[n].question;
+
+  
+  for(let i in anser){
+    
+  anser[i].innerHTML = questions[n].answer[i].text;
+  btnbtn[i].onclick = ()=>{
+    
+    console.log(i)
+    btnbtnColor("white");
+    btnbtn[i].style.color = "red";
+    (questions[n].answer[i].correct === true) ? score++ : score;
+    
+    console.log(score)
+    
+    
+  }
+}
+}
+
+function real (name){
+  return `Welcome ${name ?? "Timothy"}`
+}
+//console.log(real())
+
+function start(){
+  display();
+}
+
+start();
+  
+  
+//console.log(questions[1].answer.cor)
+
+
+
+
+
+
+
